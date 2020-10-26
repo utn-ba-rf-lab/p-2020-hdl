8c2ad27d0911a06df8c14645d79387d20ed4602a
Lucas Matias Manfredi <lmanfredi@est.frba.utn.edu.ar>
2020-10-26 20:49:04 -0300

    Agrego modulo async(que es para comunicacion de RS232 de la breakout),  un top_level modificado(que tiene intencion de hacer un blink comandado por el gnu radio), y el archivo Mercurial.py que describe el funcionamiento del GNUradio modificado

--- hdl/top/top_level.v
+++ hdl/top/top_level.v
@@ -20,7 +20,7 @@ module top_level (
     output  pwm_diff_n,
     output  pwm_pin,
     
-//    output  sdata,
+    //output  sdata,
     output  bclk,
     output  nsync,
 
@@ -28,10 +28,10 @@ module top_level (
     output  symb_clk,
     output  fifo_empty,
 
-//    output  fake_pwm,
-//    output  fake_sdata,
-//    output  fake_bclk,
-//    output  fake_nsync
+//     output  fake_pwm,
+//     output  fake_sdata,
+//     output  fake_bclk,
+//     output  fake_nsync
 );
     /***************************************************************************
      * signals
@@ -57,6 +57,11 @@ module top_level (
     reg [26:0] count;
     wire pwm_signal, sdata_signal, bclk_signal, nsync_signal;
 
+    //RS232
+    wire RxD_data_ready;
+    wire [7:0] RxD_data;
+    wire RxD_idle;
+    wire RxD_endofpacket;
     /***************************************************************************
      * assignments
      ***************************************************************************
@@ -66,15 +71,15 @@ module top_level (
     assign pwm_diff_n = ~pwm_signal;
     assign pwm_pin = pwm_signal;
 
-//    assign sdata = sdata_signal;
+    assign sdata = sdata_signal;
     assign bclk = bclk_signal;
     assign nsync = nsync_signal;
 
     // fake testing signals
-//    assign fake_pwm = pwm_signal;//read_sample;
-//    assign fake_sdata = sdata_signal;//read_sample;
-//    assign fake_bclk = bclk_signal;//rx_valid_si;
-//    assign fake_nsync = nsync_signal;//nsync_signal;
+//     assign fake_pwm = pwm_signal;//read_sample;
+//     assign fake_sdata = sdata_signal;//read_sample;
+//     assign fake_bclk = bclk_signal;//rx_valid_si;
+//     assign fake_nsync = nsync_signal;//nsync_signal;
 
     /***************************************************************************
      * module instances
@@ -90,89 +95,108 @@ module top_level (
 
     /* FT245 wrapper
      * Interface between FT245 FIFO and SIMPLE INTERFACE
-     */
-    ft245_block ft245_wrapper (
-        .clk        (clk),
-        .rst        (rst),
-        // FT245 interface
-        .in_out_245 (in_out_245),
-        .rxf_245    (rxf_245),
-        .rx_245     (rx_245),
-        .txe_245    (txe_245),
-        .wr_245     (wr_245),
-        // simple interface - RX
-        .rx_data_si (rx_data_si),
-        .rx_valid_si(rx_valid_si),
-        .rx_ready_si(!fifo_full),
-        // simple interface - TX
-        .tx_data_si (),
-        .tx_valid_si(),
-        .tx_ready_si()
-    );
+    //  */
+    // ft245_block ft245_wrapper (
+    //     .clk        (clk),
+    //     .rst        (rst),
+    //     // FT245 interface
+    //     .in_out_245 (in_out_245),
+    //     .rxf_245    (rxf_245),
+    //     .rx_245     (rx_245),
+    //     .txe_245    (txe_245),
+    //     .wr_245     (wr_245),
+    //     // simple interface - RX
+    //     .rx_data_si (rx_data_si),
+    //     .rx_valid_si(rx_valid_si),
+    //     .rx_ready_si(!fifo_full),
+    //     // simple interface - TX
+    //     .tx_data_si (),
+    //     .tx_valid_si(),
+    //     .tx_ready_si()
+    // );
 
-    fifo #(
-        .DEPTH_WIDTH    (8),
-        .DATA_WIDTH     (8)
-    ) data_fifo (
-        .clk        (clk),
-        .rst        (rst),
-        /* write port */
-        .wr_data_i  (rx_data_si),
-        .wr_en_i    (rx_valid_si & !fifo_full),
-        /* read port */
-        .rd_data_o  (sample),
-        .rd_en_i    (read_sample),
-        /* control signal */
-        .full_o     (fifo_full),
-        .empty_o    (fifo_empty)
-        );
-
-
+    // fifo #(
+    //     .DEPTH_WIDTH    (8),
+    //     .DATA_WIDTH     (8)
+    // ) data_fifo (
+    //     .clk        (clk),
+    //     .rst        (rst),
+    //     /* write port */
+    //     .wr_data_i  (rx_data_si),
+    //     .wr_en_i    (rx_valid_si & !fifo_full),
+    //     /* read port */
+    //     .rd_data_o  (sample),
+    //     .rd_en_i    (read_sample),
+    //     /* control signal */
+    //     .full_o     (fifo_full),
+    //     .empty_o    (fifo_empty)
+    //     );
+        
+        
     blinky blink(
         .clk  (clk),
         .led  (led_reg[4])
     );
-//    modulator #(
-//        .PARAMETER01    (`PARAMETER01),
-//        .PARAMETER02    (`PARAMETER02),
-//        .PARAMETER03    (`PARAMETER03),
-//        .PARAMETER04    (`PARAMETER04)
- //       /*  AM_CLKS_PER_PWM_STEP    1
-//         *  AM_PWM_STEP_PER_SAMPLE  255
-//         *  AM_BITS_PER_SAMPLE      8
-//         *  AM_REPEATED_SAMPLE      30
-//         */
-//
-//        /*  PSK_CLKS_PER_BIT        4
- //        *  PSK_BITS_PER_SYMBOL     4
-//         *
-//         *  PSK_REPEATED_SAMPLE     30
-//         */
-//
-//        /*  PAM_CLKS_SAMPLING_FREQ  1200
-//         *  PAM_CLKS_PER_BCLK       12
-//         *  PAM_DATA_LENGHT         24
-//         *  
-//         */
-//    ) top_modulator (
-//        .clk    (clk),
-//        .rst    (rst),
- //       .enable (1'b1),
- //       /* FIFO interface */
- //       .sample (sample),
-  //      .empty  (fifo_empty),
-  //      .read   (read_sample),
-  //      /* data flow */
-  //      .pwm    (pwm_signal),
- //       .sdata  (sdata_signal),
- //       .bclk   (bclk_signal),
-  //      .nsync  (nsync_signal),
-//
-//        /* test */
- //       .symb_clk(symb_clk)
- //   );
 
-    /**
+    async_receiver receive232(
+        .clk            (clk),
+        .RxD            (in_out_245[1]),    //en realidad es RS232_Rx_TTL pero los chicos lo cambiaron
+        .RxD_data_ready (RxD_data_ready),
+        .RxD_data       (RxD_data),
+        .RxD_idle       (RxD_idle),
+        .RxD_endofpacket(RxD_endofpacket)
+    );
+    always @(posedge RxD_data_ready) begin
+        if (RxD_data == 8'b1111111) begin
+            led_reg[5] = 1;
+        end
+        else if (RxD_data == 8'b00000000) begin
+            led_reg[5] = 0;
+        end
+    end
+    /*
+    MODULATOR #(
+        .PARAMETER01    (`PARAMETER01),
+        .PARAMETER02    (`PARAMETER02),
+        .PARAMETER03    (`PARAMETER03),
+        .PARAMETER04    (`PARAMETER04)
+        /*  AM_CLKS_PER_PWM_STEP    1
+         *  AM_PWM_STEP_PER_SAMPLE  255
+         *  AM_BITS_PER_SAMPLE      8
+         *  AM_REPEATED_SAMPLE      30
+         */
+
+        /*  PSK_CLKS_PER_BIT        4
+         *  PSK_BITS_PER_SYMBOL     4
+         *
+         *  PSK_REPEATED_SAMPLE     30
+         */
+
+        /*  PAM_CLKS_SAMPLING_FREQ  1200
+         *  PAM_CLKS_PER_BCLK       12
+         *  PAM_DATA_LENGHT         24
+         *  
+         */
+    /*
+    ) TOP_MODULATOR (
+        .CLK    (CLK),
+        .RST    (RST),
+        .ENABLE (1'B1),*/
+        /* FIFO INTERFACE */
+        /*.SAMPLE (SAMPLE),
+        .EMPTY  (FIFO_EMPTY),
+        .READ   (READ_SAMPLE),*/
+        /* DATA FLOW */
+       /* .PWM    (PWM_SIGNAL),
+        .SDATA  (SDATA_SIGNAL),
+        .BCLK   (BCLK_SIGNAL),
+        .NSYNC  (NSYNC_SIGNAL),*/
+
+        /* TEST */
+    /*    .SYMB_CLK(SYMB_CLK)
+    );*/
+
+    /*
      * We are using the leds to let the user know the current
      * transmitter status
      *
