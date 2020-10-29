f4d736d269ae3b91f1d7b570ffb834dc0e025ee7
Marcelo Doallo <m.doallo@frba.utn.edu.ar>
2020-10-29 20:34:42 -0300

    borrado:        hdl/docker/.Dockerfile.swp 	modificado:     hdl/docker/Dockerfile 	modificado:     hdl/run_in_docker.sh

--- hdl/run_in_docker.sh
+++ hdl/run_in_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 GIT_DIR=$(git rev-parse --show-toplevel)
-#docker build -f $GIT_DIR/docker/Dockerfile --build-arg REBUILD=`date +%s` -t icestorm .
+#docker build -f $GIT_DIR/hdl/docker/Dockerfile --build-arg REBUILD=`date +%s` -t icestorm .
 
 docker run -it --rm \
            -v $PWD:$PWD \
