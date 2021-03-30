74999a3ee770d1d66d289f93b2e5773ffcc34058
Marcelo Doallo <m.doallo@frba.utn.edu.ar>
2021-03-30 07:35:37 -0300

    nuevo archivo:  hdl/build/top_level.asc 	nuevo archivo:  hdl/build/top_level.bin 	nuevo archivo:  hdl/build/top_level.blif 	nuevo archivo:  hdl/build/top_level.json 	nuevo archivo:  hdl/top/top_level-save.v 	nuevo archivo:  test/top_level.v

---
+++ hdl/top/top_level-save.v
@@ -0,0 +1,73 @@
+`include "../inc/module_params.v"
+
+module top_level (
+    // basics
+    input   hwclk,
+    input   rst,
+
+    // i am alive
+    output  [7:0] leds,
+
+    // FT245 interface
+    inout   [7:0] in_out_245,
+    input   rxf_245,
+    output  rx_245,
+    input   txe_245,
+    output  wr_245
+);
+/***************************************************************************
+                                    signals
+***************************************************************************/
+    reg clk;
+    reg [7:0] led_reg;
+
+    //RS232
+    wire RxD_data_ready;
+    wire [7:0] RxD_data;
+    wire RxD_idle;
+    wire RxD_endofpacket;
+/***************************************************************************
+                                    assignments
+***************************************************************************/
+    assign leds = led_reg;
+
+
+/***************************************************************************
+                                     module instances
+***************************************************************************/
+    
+    /* pll */
+    pll system_clk(
+        .clock_in   (hwclk),        // 12 mhz
+        .clock_out  (clk),          // 120 mhz
+        .locked     (aux)
+    );
+
+/************************** CAMBIAR LED BLINKY **************/       
+    blinky blink(
+        .clk  (clk),
+        .led  (led_reg[3])
+    );
+
+    async_receiver receive232(
+        .clk            (clk),
+        .RxD            (in_out_245[0]),    //en realidad es RS232_Rx_TTL pero los chicos lo cambiaron
+        .RxD_data_ready (RxD_data_ready),
+        .RxD_data       (RxD_data),
+        .RxD_idle       (RxD_idle),
+        .RxD_endofpacket(RxD_endofpacket)
+    );
+    always @(posedge clk) 
+    begin
+        if (RxD_data_ready == 1) begin
+            if (RxD_data == 8'b00110001) begin
+                led_reg[2] = 1;
+            end
+            else if (RxD_data == 8'b00110000) begin
+                led_reg[2] = 0;
+            end
+        end
+    end
+ 
+
+endmodule
